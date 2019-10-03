146422db205559da9a2bdcd9799b042b51a6d71b
dduportal <1522731+dduportal@users.noreply.github.com>
2019-10-03 18:01:54 +0200

    wip worktree
    Signed-off-by: dduportal <1522731+dduportal@users.noreply.github.com>

--- scripts/travis-gh-deploy.sh
+++ scripts/travis-gh-deploy.sh
@@ -9,8 +9,13 @@ echo "== Using git ref. ${TRAVIS_BRANCH}"
 
 # Rebuild the docs directory which will be uploaded to gh-pages
 rm -rf "${DOCS_DIR}"
-time git fetch --unshallow origin "${DEPLOY_BRANCH}"
-time git worktree add --force "${DOCS_DIR}" "origin/${DEPLOY_BRANCH}"
+git config remote.origin.fetch +refs/heads/*:refs/remotes/origin/*;
+git fetch --unshallow origin gh-pages;
+git worktree add -b gh-pages "${DOCS_DIR}" origin/gh-pages;
+ls -ltr "${DOCS_DIR}/"
+exit 1
+# time git fetch --unshallow origin "${DEPLOY_BRANCH}"
+# time git worktree add --force "${DOCS_DIR}" "origin/${DEPLOY_BRANCH}"
 
 # If a tag triggered the build, then TRAVIS_BRANCH == TRAVIS_TAG
 DEPLOY_DIR="${DOCS_DIR}/${TRAVIS_BRANCH}"
