70cd2ca25a13f47322285b8a7f5abb92dd80195f
dduportal <1522731+dduportal@users.noreply.github.com>
2019-10-03 17:27:58 +0200

    Use worktree for gh-pages for faster deploy
    Signed-off-by: dduportal <1522731+dduportal@users.noreply.github.com>

--- scripts/travis-gh-deploy.sh
+++ scripts/travis-gh-deploy.sh
@@ -2,28 +2,26 @@
 
 set -eux
 
-ZIP_FILE=gh-pages.zip
-ARCHIVE_URL="${REPOSITORY_BASE_URL}/archive/${ZIP_FILE}"
 CURRENT_DIR="$(cd "$(dirname "${0}")" && pwd -P)"
 DOCS_DIR="${CURRENT_DIR}/../docs"
+DEPLOY_BRANCH="gh-pages"
+echo "== Using git ref. ${TRAVIS_BRANCH}"
 
 # Rebuild the docs directory which will be uploaded to gh-pages
 rm -rf "${DOCS_DIR}"
-if curl -sSLI --fail "${ARCHIVE_URL}"
+if git branch --remotes | grep "origin/${DEPLOY_BRANCH}"
 then
-    time curl -sSLO "${ARCHIVE_URL}"
-    unzip -o "./${ZIP_FILE}"
-    mv ./"$(basename "${REPOSITORY_BASE_URL}")"-${ZIP_FILE%%.*} "${DOCS_DIR}" # No ".zip" at the end
-    rm -f "./${ZIP_FILE}"
+    time git worktree add --force "${DOCS_DIR}" "${DEPLOY_BRANCH}"
 else
     echo "== No gh-pages found, I assume this is the first time."
     mkdir -p "${DOCS_DIR}"
 fi
 
 # If a tag triggered the build, then TRAVIS_BRANCH == TRAVIS_TAG
-echo "== Using git ref. ${TRAVIS_BRANCH}"
 DEPLOY_DIR="${DOCS_DIR}/${TRAVIS_BRANCH}"
 mkdir -p "${DEPLOY_DIR}"
+
+# TODO: update index.html
 
 # Generate QRCode and overwrite the default one
 make qrcode
