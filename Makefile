78315bcefddb5e7fb19c08340ce00929b12f9c66
Damien Duportal <damien@containo.us>
2019-10-04 15:06:36 +0200

    Tools/improvements (#16)
    * CI: do not repeat make pdf for each provider + do not use branch deploy when on a tag
    
    Signed-off-by: dduportal <1522731+dduportal@users.noreply.github.com>
    
    * CSS fix around font
    
    Signed-off-by: dduportal <1522731+dduportal@users.noreply.github.com>
    
    * First try at caching Docker images
    
    Signed-off-by: dduportal <1522731+dduportal@users.noreply.github.com>
    
    * Fix travis deploy script when branch has a slash in its name
    
    Signed-off-by: dduportal <1522731+dduportal@users.noreply.github.com>
    
    * Remove cache + update dockerfile maintainer
    
    Signed-off-by: dduportal <1522731+dduportal@users.noreply.github.com>
    
    * Switch to docker image
    
    Signed-off-by: dduportal <1522731+dduportal@users.noreply.github.com>
    
    * Add timing
    
    Signed-off-by: dduportal <1522731+dduportal@users.noreply.github.com>
    
    * Use worktree for gh-pages for faster deploy
    
    Signed-off-by: dduportal <1522731+dduportal@users.noreply.github.com>
    
    * Fix gh-pages remote cleanup
    
    Signed-off-by: dduportal <1522731+dduportal@users.noreply.github.com>
    
    * Travis need ref to be fetched before worktree
    
    Signed-off-by: dduportal <1522731+dduportal@users.noreply.github.com>
    
    * wip worktree
    
    Signed-off-by: dduportal <1522731+dduportal@users.noreply.github.com>
    
    * wip worktree
    
    Signed-off-by: dduportal <1522731+dduportal@users.noreply.github.com>
    
    * wip worktree
    
    Signed-off-by: dduportal <1522731+dduportal@users.noreply.github.com>
    
    * Deploy: Factorize heavy resources as images or video by name
    
    Signed-off-by: dduportal <1522731+dduportal@users.noreply.github.com>
    
    * Deploy: sanitize branch name from slashes to avoid nested directories
    
    Signed-off-by: dduportal <1522731+dduportal@users.noreply.github.com>
    
    * Deploy: factorize resources when deploying to gh-pages
    
    Signed-off-by: dduportal <1522731+dduportal@users.noreply.github.com>
    
    * Rollback image change
    
    Signed-off-by: dduportal <1522731+dduportal@users.noreply.github.com>
    
    * Rollback style
    
    Signed-off-by: dduportal <1522731+dduportal@users.noreply.github.com>

--- Makefile
+++ Makefile
@@ -13,23 +13,23 @@ endif
 REPOSITORY_URL = $(REPOSITORY_BASE_URL)/tree/$(TRAVIS_BRANCH)
 PRESENTATION_URL = https://$(REPOSITORY_OWNER).github.io/$(REPOSITORY_NAME)/$(TRAVIS_BRANCH)
 
-export PRESENTATION_URL CURRENT_UID REPOSITORY_URL REPOSITORY_BASE_URL
+export PRESENTATION_URL CURRENT_UID REPOSITORY_URL REPOSITORY_BASE_URL TRAVIS_BRANCH
 
-all: clean build verify
+all: clean build verify pdf
 
 # Generate documents inside a container, all *.adoc in parallel
 build: clean $(DIST_DIR)
-	@docker-compose up \
+	@time docker-compose up \
 		--build \
 		--force-recreate \
 		--exit-code-from build \
-	build
+		build
 
 $(DIST_DIR):
 	mkdir -p $(DIST_DIR)
 
 verify: $(DIST_DIR)/index.html
-	@docker run --rm \
+	@time docker run --rm \
 		-v $(DIST_DIR):/dist \
 		--user $(CURRENT_UID) \
 		18fgsa/html-proofer \
@@ -46,7 +46,7 @@ shell: $(DIST_DIR)
 	@docker-compose exec --user root wait sh
 
 pdf: $(DIST_DIR)/index.html
-	@docker run --rm -t \
+	@time docker run --rm -t \
 		-v $(DIST_DIR):/slides \
 		--user $(CURRENT_UID) \
 		--read-only=true \
@@ -56,8 +56,8 @@ pdf: $(DIST_DIR)/index.html
 		/slides/slides.pdf \
 		--size='2048x1536'
 
-deploy: pdf
-	@bash $(CURDIR)/scripts/travis-gh-deploy.sh
+deploy:
+	@time bash $(CURDIR)/scripts/travis-gh-deploy.sh
 
 clean:
 	@docker-compose down -v --remove-orphans
--- scripts/travis-gh-deploy.sh
+++ scripts/travis-gh-deploy.sh
@@ -2,29 +2,50 @@
 
 set -eux
 
-ZIP_FILE=gh-pages.zip
-ARCHIVE_URL="${REPOSITORY_BASE_URL}/archive/${ZIP_FILE}"
 CURRENT_DIR="$(cd "$(dirname "${0}")" && pwd -P)"
 DOCS_DIR="${CURRENT_DIR}/../docs"
-
-# Rebuild the docs directory which will be uploaded to gh-pages
-rm -rf "${DOCS_DIR}"
-if curl -sSLI --fail "${ARCHIVE_URL}"
-then
-    curl -sSLO "${ARCHIVE_URL}"
-    unzip -o "./${ZIP_FILE}"
-    mv ./"$(basename "${REPOSITORY_BASE_URL}")"-${ZIP_FILE%%.*} "${DOCS_DIR}" # No ".zip" at the end
-    rm -f "./${ZIP_FILE}"
-else
-    echo "== No gh-pages found, I assume this is the first time."
-    mkdir -p "${DOCS_DIR}"
-fi
+DEPLOY_BRANCH="gh-pages"
+DIST_DIR="${CURRENT_DIR}/../dist"
 
 # If a tag triggered the build, then TRAVIS_BRANCH == TRAVIS_TAG
-echo "== Using git ref. ${TRAVIS_BRANCH}"
-DEPLOY_DIR="${DOCS_DIR}/${TRAVIS_BRANCH}"
+# We replace / by _ to avoid nested directories
+CURRENT_REFERENCE="$(echo "${TRAVIS_BRANCH}" | tr '/' '_')"
 
-# Generate QRCode and overwrite the default one
+echo "== Using git reference '${TRAVIS_BRANCH}'"
+
+rm -rf "${DOCS_DIR}"
+git config remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
+git fetch --unshallow origin "${DEPLOY_BRANCH}" 2>/dev/null  || git fetch origin
+git worktree add -b "${DEPLOY_BRANCH}" "${DOCS_DIR}" origin/"${DEPLOY_BRANCH}" 2>/dev/null || git worktree add --force "${DOCS_DIR}" origin/"${DEPLOY_BRANCH}" 2>/dev/null
+
+DEPLOY_DIR="${DOCS_DIR}/${CURRENT_REFERENCE}"
+mkdir -p "${DEPLOY_DIR}"
+
+# TODO: update index.html
+
+# Generate QRCode to ./dist/images/qrcode.png and overwrite the default one
 make qrcode
 
-rsync -av ./dist/ "${DEPLOY_DIR}"
+# Get current branch dist dir to docs root
+rsync -av "${DIST_DIR}/" "${DEPLOY_DIR}"
+
+### Factorize resources already existing
+# Factorization is done on filename: if you don't name your file correctly it will be overriden
+# (reason: if any image optim happens on branches, then the same image will be duplicated for nothing)
+for resource_dir in videos images fonts
+do
+    common_resource_dir="${DOCS_DIR}/${resource_dir}"
+    for resource in "${DEPLOY_DIR}/${resource_dir}"/*
+    do
+        resource_file="$(basename "${resource}")"
+        if [ -f "${common_resource_dir}/${resource_file}" ]
+        then
+            # if we find a resource with the same name in the corresponding "common resource dir", we factorize by linking it
+            pushd "${DEPLOY_DIR}/${resource_dir}"
+            rm -f "${resource_file}"
+            # We expect only 2 levels of directories!
+            ln -s "../../${resource_dir}/${resource_file}" "${resource_file}"
+            popd
+        fi
+    done
+done
